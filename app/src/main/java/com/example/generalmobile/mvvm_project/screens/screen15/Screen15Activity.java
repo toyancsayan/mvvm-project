ab1ddf2eb044cdb4d33b3e31be5ae2a02eb86f4c
toyancsayan <toyancsayan@gmail.com>
2017-07-26 18:24:57 +0300

    Layouts and Bindings are completed.

--- app/src/main/java/com/example/generalmobile/mvvm_project/screens/screen15/Screen15Activity.java
+++ app/src/main/java/com/example/generalmobile/mvvm_project/screens/screen15/Screen15Activity.java
@@ -3,9 +3,15 @@ package com.example.generalmobile.mvvm_project.screens.screen15;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.generalmobile.mvvm_project.R;
 import com.example.generalmobile.mvvm_project.base.BaseActivity;
+import com.example.generalmobile.mvvm_project.databinding.ActivityScreen15Binding;
 
 /**
  * Created by btstajyer7 on 25.07.2017.
@@ -18,5 +24,43 @@ public class Screen15Activity extends BaseActivity implements android.databindin
 
         DataBindingUtil.setContentView(this, R.layout.activity_screen15);
         super.onCreate(savedInstanceState);
+        ActivityScreen15Binding binding = DataBindingUtil.setContentView(this, R.layout.activity_screen15);
+        Screen15ViewModel model = new Screen15ViewModel();
+        binding.setModel(model);
+
+        // setAdapter();
     }
+
+    public void makeToast(String option) {
+        Toast.makeText(this, option, Toast.LENGTH_SHORT).show();
+    }
+
+    private void setAdapter() {
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinnerState1);
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinnerState2);
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinnerState3);
+// Create an ArrayAdapter using the string array and a default spinner layout
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.options_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner1.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
+        spinner3.setAdapter(adapter);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                Toast.makeText(Screen15Activity.this, selectedItem + " is selected", Toast.LENGTH_SHORT).show();
+            } // to close the onItemSelected
+
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+
 }
--- app/src/main/java/com/example/generalmobile/mvvm_project/screens/screen15/Screen15ViewModel.java
+++ app/src/main/java/com/example/generalmobile/mvvm_project/screens/screen15/Screen15ViewModel.java
@@ -1,8 +1,87 @@
 package com.example.generalmobile.mvvm_project.screens.screen15;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.BindingAdapter;
+import android.databinding.InverseBindingAdapter;
+import android.databinding.InverseBindingListener;
+import android.databinding.InverseBindingMethod;
+import android.databinding.InverseBindingMethods;
+import android.databinding.ObservableField;
+import android.support.v7.widget.AppCompatSpinner;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.Toast;
+
+import com.example.generalmobile.mvvm_project.BR;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
 /**
  * Created by btstajyer7 on 25.07.2017.
  */
 
-public class Screen15ViewModel {
+@InverseBindingMethods({
+        @InverseBindingMethod(type = Spinner.class, attribute = "android:selectedItemPosition"),
+})
+
+
+public class Screen15ViewModel extends BaseObservable {
+
+    private String[] options = new String[]{"Option1", "Option2", "Option3", "Option4", "Option5", "Option6", "Option7"};
+    private String option;
+    private int position;
+    Screen15Activity activity = new Screen15Activity();
+
+
+    @Bindable
+    public String[] getOptions() {
+        return options;
+    }
+
+    public void setOptions(String[] countries) {
+        this.options = countries;
+
+    }
+
+    @Bindable
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+
+        if (position != 0)
+            setOption(options[position]);
+    }
+
+    @Bindable
+    public String getOption() {
+        return option;
+    }
+
+
+    public void setOption(String country) {
+        this.option = country;
+        activity.makeToast(country);
+    }
+
+    public int getPosition(Spinner spinner) {
+        return spinner.getSelectedItemPosition();
+    }
+
+
+
+
+
 }
+
